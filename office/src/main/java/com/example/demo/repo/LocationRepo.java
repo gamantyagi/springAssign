7f7e049dbba5d9a8f1d298ff72a7654068f02a18
gaman tyagi <42539220+gamantyagi@users.noreply.github.com>
2023-02-16 00:40:50 +0530

    Delete office directory

--- office/src/main/java/com/example/demo/repo/LocationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Location;
-
-public interface LocationRepo extends CrudRepository<Location, Integer> {
-
-}
